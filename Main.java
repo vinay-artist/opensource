ffbaa51abf5f11fdedb2df299e8bbb38fec18c43
vinay-artist <vinay9502316960@gmail.com>
2024-11-12 10:29:15 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class hellodemo
+  {
+    public ststic void main(String[]args)
+    {
+      Scannner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(a * b);
+    }
+  }
